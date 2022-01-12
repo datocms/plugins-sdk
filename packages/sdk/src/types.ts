32c252251399140a6e4b7000cc3ed9aa8cfe7ae0
Stefano Verna <s.verna@datocms.com>
2022-01-12 13:14:21 +0100

    Flatten down

--- packages/sdk/src/types.ts
+++ packages/sdk/src/types.ts
@@ -304,29 +304,27 @@ export type StructuredTextCustomMark = {
   id: string;
   /** Label representing the custom mark */
   label: string;
-  toolbarButton: {
-    /**
-     * Icon to be shown alongside the label. Can be a FontAwesome icon name (ie.
-     * `"address-book"`) or a custom SVG definition. To maintain visual
-     * consistency with the rest of the interface, try to use FontAwesome icons
-     * whenever possible
-     */
-    icon: Icon;
-    /**
-     * Expresses where you want the custom mark button to be placed inside the
-     * toolbar. If not specified, the item will be placed after the standard
-     * marks provided by DatoCMS itself.
-     */
-    placement?: StructuredTextCustomMarkPlacement;
-    /**
-     * If multiple custom marks specify the same `placement` for their toolbar
-     * button, they will be sorted by ascending `rank`. If you want to specify
-     * an explicit value for `rank`, make sure to offer a way for final users to
-     * customize it inside the plugin's settings form, otherwise the hardcoded
-     * value you choose might clash with the one of another plugin!
-     */
-    rank?: number;
-  };
+  /**
+   * Icon to be shown alongside the label. Can be a FontAwesome icon name (ie.
+   * `"address-book"`) or a custom SVG definition. To maintain visual
+   * consistency with the rest of the interface, try to use FontAwesome icons
+   * whenever possible
+   */
+  icon: Icon;
+  /**
+   * Expresses where you want the custom mark button to be placed inside the
+   * toolbar. If not specified, the item will be placed after the standard marks
+   * provided by DatoCMS itself.
+   */
+  placement?: StructuredTextCustomMarkPlacement;
+  /**
+   * If multiple custom marks specify the same `placement` for their toolbar
+   * button, they will be sorted by ascending `rank`. If you want to specify an
+   * explicit value for `rank`, make sure to offer a way for final users to
+   * customize it inside the plugin's settings form, otherwise the hardcoded
+   * value you choose might clash with the one of another plugin!
+   */
+  rank?: number;
   /**
    * Keyboard shortcut associated with the custom mark, expressed using the
    * https://github.com/ianstormtaylor/is-hotkey syntax (ie. `mod+shift+x`)
