091efc513f4bf79b24816e27597e9edd1de623f6
Stefano Verna <s.verna@datocms.com>
2022-08-31 12:07:20 +0200

    Update to React 18

--- packages/cra-template/template/src/utils/render.tsx
+++ packages/cra-template/template/src/utils/render.tsx
@@ -1,6 +1,8 @@
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 export function render(component: React.ReactNode): void {
-  ReactDOM.render(<StrictMode>{component}</StrictMode>, document.getElementById('root'));
+  const container = document.getElementById('root');
+  const root = createRoot(container!);
+  root.render(<StrictMode>{component}</StrictMode>);
 }
