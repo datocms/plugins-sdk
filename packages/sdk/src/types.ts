9a9cf72f18757f0c47955289366607cf9d4b0484
Stefano Verna <s.verna@datocms.com>
2022-11-03 11:54:49 +0100

    Include information about the block

--- packages/sdk/src/types.ts
+++ packages/sdk/src/types.ts
@@ -1276,6 +1276,12 @@ export type RenderFieldExtensionAdditionalProperties = {
    * level Modular Content/Structured Text field containing the block itself
    */
   parentField: Field | undefined;
+  /**
+   * If the field extension is installed in a field of a block, returns the ID
+   * of the block — or `undefined` if the block is still not persisted — and the
+   * block model.
+   */
+  block: undefined | { id: string | undefined; blockModel: ItemType };
 };
 
 export type RenderFieldExtensionProperties = ItemFormProperties &
